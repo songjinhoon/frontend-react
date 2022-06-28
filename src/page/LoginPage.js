b0bdc0289924aa1a58a7fb6f00333b51ff6de049
hijinhoon <hijinhoon@muhanit.kr>
2022-06-28 17:35:50 +0900

    init

---
+++ src/page/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '../componet/common/Button';
+
+const LoginPage = () => {
+  return (
+    <>
+      <div>로그인</div>
+      <Button>버튼</Button>
+    </>
+  );
+};
+
+export default LoginPage;
