511a601ee6aff9dd9a0abeb21c162e1682459830
hijinhoon <hijinhoon@muhanit.kr>
2022-06-29 13:14:02 +0900

    프론트 기본 설정

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from '../node_modules/react-router-dom/index';
+import rootReducer from './module/index';
+import { composeWithDevTools } from '../node_modules/redux-devtools-extension/index';
+import { Provider } from '../node_modules/react-redux/es/exports';
+import { legacy_createStore as createStore } from 'redux';
 
+const store = createStore(rootReducer, composeWithDevTools());
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
 
