23ce4b9fd2402d36fe0d4252b015228cc0ecd45b
hijinhoon <hijinhoon@muhanit.kr>
2022-07-08 13:51:29 +0900

    feat: header ui 작업

--- src/componet/common/Button.js
+++ src/componet/common/Button.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import palette from '../../lib/styles/palette';
+import { Link } from 'react-router-dom';
 
-const StyledButton = styled.button`
+const buttonStyle = css`
   border: none;
   border-radius: 4px;
   font-size: 1rem;
@@ -36,6 +37,16 @@ const StyledButton = styled.button`
     `}
 `;
 
-const Button = (props) => <StyledButton {...props} />;
+const StyledButton = styled.button`
+  ${buttonStyle}
+`;
+
+const StyledLink = styled(Link)`
+  ${buttonStyle}
+`;
+
+const Button = (props) => {
+  return props.to ? <StyledLink {...props} cyan={props.cyAN ? 1 : 0}></StyledLink> : <StyledButton {...props}></StyledButton>;
+};
 
 export default Button;
